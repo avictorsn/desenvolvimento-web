97b478692d938e8370690e6f3565171bb9f75351
Victor Santiago <antoniovictors@gmail.com>
2019-10-24 11:51:35 -0300

    Adicionando páginas, components e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { AuthenticationPageComponent } from './pages/authentication-page/authentication-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {path: 'home', component: HomePageComponent},
+  {path: 'signup', component: SignupPageComponent},
+  {path: 'auth', component: AuthenticationPageComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthenticationPageComponent } from './pages/authentication-page/authentication-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent,
+    SignupPageComponent,
+    NavbarComponent,
+    AuthenticationPageComponent
   ],
   imports: [
     BrowserModule,
